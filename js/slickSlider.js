7165b12be9e04a8ea11da9e9a27aa9e974ed8947
Alexandr Andreev <andreevalser17@gmail.com>
2017-11-18 21:15:29 +0200

    Add slick slider

---
+++ js/slickSlider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $('.online_store').slick({
+        draggable: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        touchThreshold: 1000
+    });
+});
